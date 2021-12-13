3647c7e25248b3633e5511163696a4d63c6bc923
Greg <aquamirror@gmail.com>
2021-12-12 16:01:01 -0800

    whoa like whoa
    kinda feeling like this is too easy

--- server.js
+++ server.js
@@ -2,3 +2,7 @@ const PORT = process.env.PORT || 3001;
 const fs = require('fs');
 const path = require('path');
 
+const express = require('express');
+const app = express();
+
+const allNotes = require('./db/db.json');
