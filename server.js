a8062cdf96ca955f71f4e405867b317f9aebbbe8
Greg <aquamirror@gmail.com>
2021-12-12 16:56:57 -0800

    Update server.js

--- server.js
+++ server.js
@@ -6,3 +6,12 @@ const express = require('express');
 const app = express();
 
 const allNotes = require('./db/db.json');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res)  => {
+    res.json(allNotes.slice(1));
+});
+
