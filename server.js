266f02f33e8540871adc6ef303a984e88e1b3cd3
Greg <aquamirror@gmail.com>
2021-12-12 23:49:18 -0800

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/api/notes', (req, res)  => {
+app.get('/api/notes', (req, res) => {
     res.json(allNotes.slice(1));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
